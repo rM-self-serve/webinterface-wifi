c344e3127fe35ba51c3579b3efdc08c1ef28dfd4
rM-self-serve <122753594+rM-self-serve@users.noreply.github.com>
2023-01-17 22:58:19 -0600

    install to /home/root/.local/bin

--- install-webint-wf.sh
+++ install-webint-wf.sh
@@ -1,12 +1,29 @@
 echo "View the web interface over wifi"
+echo "This program will be installed in /home/root/.local/bin"
+echo "The path will automatically be added to .bashrc"
+
+read -r -p "Would you like to continue with installation? [y/N] " response
+case "$response" in
+[yY][eE][sS] | [yY])
+    echo "Installing webinterface-wifi"
+    ;;
+*)
+    echo "exiting installer" && exit
+    ;;
+esac
+
+mkdir -p /home/root/.local/bin
+GNU nano 4.9.3 try.sh
+if [[ $PATH != *"/home/root/.local/bin"* ]]; then
+    echo 'export PATH=$PATH:/home/root/.local/bin' >> /home/root/.bashrc
+fi
 
 wget https://github.com/rM-self-serve/webinterface-wifi/releases/download/v1.0.0/webinterface-wifi \
--P /usr/bin
+    -P /home/root/.local/bin
 
-chmod +x /usr/bin/webinterface-wifi
+chmod +x /home/root/.local/bin/webinterface-wifi
 
 wget https://raw.githubusercontent.com/rM-self-serve/webinterface-wifi/master/webinterface-wifi.service \
- -P /lib/systemd/system
+    -P /lib/systemd/system
 
 systemctl daemon-reload
-systemctl enable webinterface-wifi --now
--- remove-webint-wf.sh
+++ remove-webint-wf.sh
@@ -1,6 +1,17 @@
 echo "Remove webinterface-wifi"
+echo "This will not remove the /home/root/.local/bin directory nor the path from .bashrc"
 
-rm /usr/bin/webinterface-wifi
+read -r -p "Would you like to continue with removal? [y/N] " response
+case "$response" in
+[yY][eE][sS] | [yY])
+    echo "Removing webinterface-wifi"
+    ;;
+*)
+    echo "exiting removal" && exit
+    ;;
+esac
+
+rm /home/root/.local/bin/webinterface-wifi
 
 systemctl disable webinterface-wifi --now
 
