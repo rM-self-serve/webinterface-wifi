2ed757dcd1fad2b0366cc451e22ccb6fa9f19143
rM-self-serve <122753594+rM-self-serve@users.noreply.github.com>
2025-01-13 14:45:17 -0600

    rMpp support

--- src/netinfo/ssid_cmd.rs
+++ src/netinfo/ssid_cmd.rs
@@ -4,7 +4,16 @@ use std::process::{Command, Stdio};
 use crate::constants::CMD_ENV;
 
 pub fn get_ssid(wifi_interface: &str) -> std::io::Result<Option<String>> {
-    for line in cmd_ssid(wifi_interface)?.lines() {
+    if let Ok(Some(v)) = get_iw_ssid(wifi_interface) {
+        return Ok(Some(v));
+    }
+
+    get_wpa_cli_ssid(wifi_interface)
+}
+
+/// RM1, RM2
+fn get_iw_ssid(wifi_interface: &str) -> std::io::Result<Option<String>> {
+    for line in cmd_iw_ssid(wifi_interface)?.lines() {
         if line.contains("ssid") {
             if let Some(val) = line.split_whitespace().nth(1) {
                 return Ok(Some(val.to_string()));
@@ -14,9 +23,31 @@ pub fn get_ssid(wifi_interface: &str) -> std::io::Result<Option<String>> {
     Ok(None)
 }
 
-fn cmd_ssid(wifi_interface: &str) -> std::io::Result<String> {
+/// RM1, RM2
+fn cmd_iw_ssid(wifi_interface: &str) -> std::io::Result<String> {
     let command_out = Command::new(CMD_ENV)
         .args(["iw", "dev", wifi_interface, "info"])
+        .stdout(Stdio::piped())
+        .output()?;
+    String::from_utf8(command_out.stdout).map_err(|err| Error::new(ErrorKind::Other, err))
+}
+
+/// RMPP
+fn get_wpa_cli_ssid(wifi_interface: &str) -> std::io::Result<Option<String>> {
+    for line in cmd_wpa_cli_ssid(wifi_interface)?.lines() {
+        if line.starts_with("ssid") {
+            if let Some(val) = line.split("=").nth(1) {
+                return Ok(Some(val.to_string()));
+            }
+        }
+    }
+    Ok(None)
+}
+
+/// RMPP
+fn cmd_wpa_cli_ssid(wifi_interface: &str) -> std::io::Result<String> {
+    let command_out = Command::new(CMD_ENV)
+        .args(["wpa_cli", "status", "-i", wifi_interface])
         .stdout(Stdio::piped())
         .output()?;
     String::from_utf8(command_out.stdout).map_err(|err| Error::new(ErrorKind::Other, err))
